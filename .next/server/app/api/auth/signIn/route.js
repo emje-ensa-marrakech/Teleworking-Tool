a5877b8bf1b4a40b450bf9b882af3e3fc90d371f
chaimaa654 <chaimaaelgadaoui@gmail.com>
2025-03-07 05:36:38 +0000

    Removed .next from tracking

--- .next/server/app/api/auth/signIn/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/auth/signIn/route.js";
-const runtime = require("../../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/node_modules_fef46edf._.js");
-runtime.loadChunk("server/chunks/[root of the server]__3280a957._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/auth/signIn/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/auth/signIn/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/auth/signIn/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
